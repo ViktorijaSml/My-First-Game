b932d181c248e9be1b1513f0e303f1451fa96d7d
ViktorijaSml <vsmlatic@gmail.com>
2022-11-08 12:39:46 +0100

    Added Game project

---
+++ Game/Assets/AboutEvent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class AboutEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Animator Anim;
+    public void OnPointerEnter(PointerEventData pointer) {
+        Debug.Log("Enter");
+        Anim.SetBool("Zoom", true);
+    }
+
+    public void OnPointerExit(PointerEventData pointer) {
+        Debug.Log("Exit");
+        Anim.SetBool("Zoom", false);
+    }
+}
